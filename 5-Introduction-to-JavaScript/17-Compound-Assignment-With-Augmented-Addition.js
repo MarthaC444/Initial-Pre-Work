65d9bafc85c29148c4f08126e30fe614b9de61ee
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-14 13:43:01 -0600

    Add compound assignment addition

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,11 @@
+// The operator += will add a number to a variable and assign the sum to that same variable.
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;
