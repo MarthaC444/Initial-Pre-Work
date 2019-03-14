95302505b6473b65b682763a188d0310d7ed2a4e
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-14 13:49:13 -0600

    Add compound assignment multiplication

---
+++ 5-Introduction-to-JavaScript/19-Compound-Assignment-With-Augmented-Multiplication.js
@@ -0,0 +1,12 @@
+// The operator "*=" will multiply a variable by a number and then assign the product to the same variable.
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only modify code below this line
+
+a *= 5;
+b *= 3;
+c *= 10;
+
