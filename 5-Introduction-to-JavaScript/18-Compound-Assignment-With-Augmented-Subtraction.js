c4d1d1f7702ca896d7aa20b35a7cf669a25f32ff
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-14 13:45:49 -0600

    Add compound assignment subtraction solution

---
+++ 5-Introduction-to-JavaScript/18-Compound-Assignment-With-Augmented-Subtraction.js
@@ -0,0 +1,12 @@
+// The operator "-=" will subract a number from a variable and assign it to the same variable.
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a -= 6;
+b -= 15;
+c -= 1;
+
