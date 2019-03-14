3c851fb56f356fa22bf53a13e35cb585ab5710f6
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-14 17:24:15 -0600

    Add understand string immutability solution

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,9 @@
+// String values are immutable. A string variable can only be changed by reassigning the whole string value.
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
+
