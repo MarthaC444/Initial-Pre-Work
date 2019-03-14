5a767b8bab93fd6deb077b2d81b8660de707bebd
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-14 14:18:45 -0600

    Add escape sequences in strings solution

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,3 @@
+// Other characters need to be escaped in a string:  to allow use of characters one can't type out, and to allow multiple quotes in a string.
+
+var myStr = 'FirstLine\n\t\\SecondLine\nThirdLine'; // Change this line
