2a8f8225c360716757d8446f0e89b0a54e85dba1
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-14 17:20:53 -0600

    Add bracket notation solution

---
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,15 @@
+// I can use bracket notation to retrieve a value from an index location.
+
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
+
