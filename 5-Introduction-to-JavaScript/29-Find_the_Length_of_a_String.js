631bd9548fbc7567ff36019c7709c8fd9b7110ed
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-14 17:15:00 -0600

    Add find the length of a string solution

---
+++ 5-Introduction-to-JavaScript/29-Find_the_Length_of_a_String.js
@@ -0,0 +1,18 @@
+// .length can be used to find the length of a string variable or a string literal. 
+
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+lastNameLength = lastName.length;
+
+
+
