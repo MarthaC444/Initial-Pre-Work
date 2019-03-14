9362b08f78c4313117f6d24e4aeafbb5adb83f29
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-14 17:03:19 -0600

    Add constructing strings with variables solution

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,10 @@
+// I can insert variables into my strings with a variable.
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Martha";
+var myStr = "My name is " + myName + "and I am well!";
+
