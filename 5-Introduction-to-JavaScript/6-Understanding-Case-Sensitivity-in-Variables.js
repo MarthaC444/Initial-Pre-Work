cfe2f6f8d11fba407f25909f42654e6c01002a18
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-14 12:52:34 -0600

    Add understanding case in variables solution

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,11 @@
+// Capitalization of letters matters in variable and function names.
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
