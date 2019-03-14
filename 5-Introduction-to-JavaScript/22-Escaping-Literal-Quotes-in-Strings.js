d442e0941b0170262acb6522c1f39e5bf7b4292c
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-14 14:01:08 -0600

    Add escaping literal quotes solution

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,4 @@
+// Inside a string, I must escape a character that means something else in Javascript so that it is read as part of the string.
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
