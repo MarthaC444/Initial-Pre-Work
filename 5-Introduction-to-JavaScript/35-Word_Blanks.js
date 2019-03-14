e45af084ac967238f325a32fd77d16f1cc572785
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-14 17:50:55 -0600

    Add word blank solution

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,12 @@
+// I can concatenate multiple strings and variables.
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+    // Your code below this line
+    var result = "There once was a fat " + myNoun + " who was so " + myAdjective + " that when he " + myVerb + " through his doggie door he would " + myAdverb + " get stuck." ;
+  
+    // Your code above this line
+    return result;
+  }
+  
+  // Change the words here to test your function
+  wordBlanks("dog", "big", "ran", "quickly");
