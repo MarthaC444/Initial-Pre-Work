461d4e7eec1beaff0b4025c098adfc7c78fa89ae
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-14 21:49:36 -0600

    Add compare with || solution

---
+++ 5-Introduction-to-JavaScript/67-Comparisons_with_the_Logical_Or_Operator.js
@@ -0,0 +1,15 @@
+// "||" means or. Returns true if either of the operands are true. 
+
+function testLogicalOr(val) {
+    // Only change code below this line
+  
+    if (val < 10 || val > 20)  {
+      return "Outside";
+    }
+  
+    // Only change code above this line
+    return "Inside";
+  }
+  
+  // Change this value to test
+  testLogicalOr(15);
