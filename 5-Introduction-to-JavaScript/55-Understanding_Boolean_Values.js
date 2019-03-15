9cb3a55bae4ee686276751b23a19ef15a00d0954
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-14 21:01:16 -0600

    Add understanding boolean values solution

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,10 @@
+// Booleans my only have one of two values: true or false.
+
+function welcomeToBooleans() {
+
+    // Only change code below this line.
+    
+    return true; // Change this line
+    
+    // Only change code above this line.
+    }
