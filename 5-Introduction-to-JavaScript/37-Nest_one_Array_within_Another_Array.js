b03652d9ac965a5ab335d1c3f2770129e5306679
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-14 18:00:00 -0600

    Add nested array solution

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,7 @@
+// Arrays can be nested in other arrays.
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = ["cow", ["persian cat", "blue cat", "fancy cat"], "rat"];
