6874834af3535e4cf3851474835f7f27cfc15e54
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-15 13:12:11 -0600

    Add returning boolean values from functions solution

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,9 @@
+// Comparison operators return true or false. Sometimes it may make sense to return the boolean value instead of doing an if else statement. 
+
+function isLess(a, b) {
+    // Fix this code
+    return a < b;
+  }
+  
+  // Change these values to test
+  isLess(10, 15);
