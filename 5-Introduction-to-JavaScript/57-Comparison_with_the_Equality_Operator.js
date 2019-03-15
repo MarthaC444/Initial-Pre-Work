9e504ba7864603952250ea9ce65066c4610b9651
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-14 21:08:47 -0600

    Add comparison with the equality operator solution

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,12 @@
+// Comparison operators return boolean values. 
+
+// Setup
+function testEqual(val) {
+    if (val == 12) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  // Change this value to test
+  testEqual(10);
