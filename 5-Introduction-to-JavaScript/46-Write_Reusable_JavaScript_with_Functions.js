11bcfe73c7c8aaf555743a4638f7241968e87366
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-14 18:47:00 -0600

    Add reusable function solution

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,15 @@
+// I can divide up my code into reusable parts called functions.
+
+// Example
+function ourReusableFunction() {
+    console.log("Heyya, World");
+  }
+  
+  ourReusableFunction();
+  
+  // Only change code below this line
+  function reusableFunction() {
+      console.log("Hi World");
+  }
+  
+  reusableFunction();
