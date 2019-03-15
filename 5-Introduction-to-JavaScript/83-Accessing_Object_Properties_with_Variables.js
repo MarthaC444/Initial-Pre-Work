aff5faf63fc39a4a2d55443c7e11648f71067a13
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-15 16:35:50 -0600

    Add accessing object properties with vaiables solution

---
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,13 @@
+// Bracket notation can also be used to access property values through a variable.
+
+// Setup
+var testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+  };
+  
+  // Only change code below this line;
+  
+  var playerNumber = 16;       // Change this Line
+  var player = testObj[playerNumber];   // Change this Line
