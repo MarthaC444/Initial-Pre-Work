b79fc3d21ad09759d0e243d20f39441b44d096e1
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-14 21:06:50 -0600

    Add use conditional logic with if statements solution

---
+++ 5-Introduction-to-JavaScript/56-Use_Conditional_Logic_with_If_Statements.js
@@ -0,0 +1,25 @@
+// If statements allow the execution of code if the statement meets certain criteria.
+
+// Example
+function ourTrueOrFalse(isItTrue) {
+    if (isItTrue) { 
+      return "Yes, it's true";
+    }
+    return "No, it's false";
+  }
+  
+  // Setup
+  function trueOrFalse(wasThatTrue) {
+  
+    // Only change code below this line.
+    if (wasThatTrue) {
+        return "Yes, that was true";
+    }
+    return "No, that was false";
+    
+    // Only change code above this line.
+  
+  }
+  
+  // Change this value to test
+  trueOrFalse(true);
