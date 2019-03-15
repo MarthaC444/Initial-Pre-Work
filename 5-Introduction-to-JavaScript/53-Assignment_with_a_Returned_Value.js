2a88dc1352b8d9b269006473c53c88158415d994
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-14 20:43:25 -0600

    Add assignment with a returned value solution

---
+++ 5-Introduction-to-JavaScript/53-Assignment_with_a_Returned_Value.js
@@ -0,0 +1,21 @@
+// I can assign the value returned by a function to a variable.
+
+// Example
+var changed = 0;
+
+function change(num) {
+  return (num + 5) / 3;
+}
+
+changed = change(10);
+
+// Setup
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+
+processed = processArg(7);
