9021c04ac1a96f84f3a9cba465dbadef5c4373ac
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-14 20:25:19 -0600

    Add return value from function solution

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,14 @@
+// I can use "return" to send value out of a function.
+
+// Example
+function minusSeven(num) {
+    return num - 7;
+  }
+  
+  // Only change code below this line
+  function timesFive(num) {
+    return num * 5;
+  }
+  
+  
+  console.log(minusSeven(10));
