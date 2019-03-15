6110bf988e11b78bfa68740f0b3da7a38a8ebf8b
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-14 21:55:14 -0600

    Add else statement solution

---
+++ 5-Introduction-to-JavaScript/68-Introducing_Else_Statements.js
@@ -0,0 +1,19 @@
+// With "else" an alternate block of code can be executd when an if statement is false.
+
+function testElse(val) {
+    var result = "";
+    // Only change code below this line
+    
+    if (val > 5) {
+      result = "Bigger than 5";
+    } else {
+      result = "5 or Smaller";
+    }
+   
+    // Only change code above this line
+    return result;
+  }
+  
+  // Change this value to test
+  testElse(4);
+  
