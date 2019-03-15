938a161624baddf29e76a0341b0abd3bb492ffab
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-15 14:15:58 -0600

    Add build javascript objects solution

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,18 @@
+// An object is a data structure that I access through properties instead of an index. All properties will be typecast as strings.
+
+// Example
+var ourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"]
+  };
+  
+  // Only change code below this line.
+  
+  var myDog = {
+    name: "Bob",
+    legs: 3,
+    tails: 1,
+    friends: ["Everyone", "Jill", "Clementine", "Spot", "Butterflies"]
+  };
