b061b42ff62d60877f80eae74e40e485b62bf2b8
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-14 20:04:01 -0600

    Add passing values with arguments solution

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,15 @@
+// Values that are passed to functions are called arguments, the placholder they are passed though is called a parameter.
+
+// Example
+function ourFunctionWithArgs(a, b) {
+    console.log(a - b);
+  }
+  ourFunctionWithArgs(10, 5); // Outputs 5
+  
+  // Only change code below this line.
+  
+  function functionWithArgs(c, d) {
+    console.log(c + d);
+  }
+  
+  functionWithArgs(2, 5);
