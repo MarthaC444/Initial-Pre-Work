0d23b1340c3512ac7be4cfdc0c0df0689930a3b1
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-16 15:21:01 -0600

    Add generate random fraction solution

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,11 @@
+// Math.random() will return a value between 0 and 1. Inclusive of 0 and exclusive of 1.
+
+
+function randomFraction() {
+
+    // Only change code below this line.
+  
+    return Math.random();
+  
+    // Only change code above this line.
+  }
