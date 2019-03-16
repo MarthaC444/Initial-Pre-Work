3b6aa859448d20f22a61f33eb4094f458a4d9448
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-15 21:39:02 -0600

    Add iterate with while loop solution

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,12 @@
+// A while loop runs as long as the condition is true and stops when it is not.
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+var i = 0;
+while (i < 5) {
+    myArray.push(i);
+    i++;
+}
