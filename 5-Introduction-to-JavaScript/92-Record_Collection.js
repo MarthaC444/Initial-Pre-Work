87004caa0031f9cfb9b9ee5ce380b03f4f858593
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-15 21:30:27 -0600

    Add record collection solution

---
+++ 5-Introduction-to-JavaScript/92-Record_Collection.js
@@ -0,0 +1,17 @@
+// Practice with if/else and accessing object properties/values. I needed the hints to solve this one.
+
+function updateRecords(id, prop, value) {
+    if (prop === "tracks" && value !== "") {
+      if (collection[id][prop]) {
+        collection[id][prop].push(value);
+      } else {
+        collection[id][prop] = [value];
+      }
+    } else if (value !== "") {
+      collection[id][prop] = value;
+    } else {
+      delete collection[id][prop];
+    }
+    
+    return collection;
+  }
