6f1ae1b2fbb66dfbc2f3d9b2634fbd32a18979d4
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-16 15:25:47 -0600

    Add generate random whole numbers solution

---
+++ 5-Introduction-to-JavaScript/102-Generate_Random_Whole_Numbers_with_JavaScript.js
@@ -0,0 +1,10 @@
+// I can create random whole numbers by multiplying the result of Math.random() by 10, 20, 30 etc. and the applying Math.floor(). 
+
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+  // Only change code below this line.
+
+  return Math.floor(Math.random() * 10);
+}
