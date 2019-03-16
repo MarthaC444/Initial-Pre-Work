522b8b9df5dcc85a53aa45a951e5c32cb8ba8d3a
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-15 21:36:10 -0600

    Add correct record collection solution

--- 5-Introduction-to-JavaScript/92-Record_Collection.js
+++ 5-Introduction-to-JavaScript/92-Record_Collection.js
@@ -1,5 +1,35 @@
 // Practice with if/else and accessing object properties/values. I needed the hints to solve this one.
 
+// Setup
+var collection = {
+    "2548": {
+      "album": "Slippery When Wet",
+      "artist": "Bon Jovi",
+      "tracks": [ 
+        "Let It Rock", 
+        "You Give Love a Bad Name" 
+      ]
+    },
+    "2468": {
+      "album": "1999",
+      "artist": "Prince",
+      "tracks": [ 
+        "1999", 
+        "Little Red Corvette" 
+      ]
+    },
+    "1245": {
+      "artist": "Robert Palmer",
+      "tracks": [ ]
+    },
+    "5439": {
+      "album": "ABBA Gold"
+    }
+};
+// Keep a copy of the collection for tests
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+
+// Only change code below this line
 function updateRecords(id, prop, value) {
     if (prop === "tracks" && value !== "") {
       if (collection[id][prop]) {
@@ -15,3 +45,6 @@ function updateRecords(id, prop, value) {
     
     return collection;
   }
+
+// Alter values below to test your code
+updateRecords(5439, "artist", "ABBA");
