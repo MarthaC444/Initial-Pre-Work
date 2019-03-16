b9d8561d98476f60b9982268a05cd4cbe43a4389
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-15 21:42:24 -0600

    Add iterate with for loops solution

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,17 @@
+// Loops allow me to run the same code multiple times. The for loop has three optional expressions.
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+for (var i = 1; i < 6; i++) {
+    myArray.push(i);
+}
