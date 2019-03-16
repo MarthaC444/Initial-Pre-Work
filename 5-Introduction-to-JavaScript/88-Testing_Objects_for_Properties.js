abe8f7d994ae3a71cc55857c9ee69dabeb119b4d
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-15 18:02:51 -0600

    Add testing objects for properties solution

---
+++ 5-Introduction-to-JavaScript/88-Testing_Objects_for_Properties.js
@@ -0,0 +1,20 @@
+// I can check to see if a property exists with hasOwnProperty().
+
+// Setup
+var myObj = {
+    gift: "pony",
+    pet: "kitten",
+    bed: "sleigh"
+  };
+  
+  function checkObj(checkProp) {
+    // Your Code Here
+    if (myObj.hasOwnProperty(checkProp) === true) {
+      return myObj[checkProp];
+    } else {
+      return "Not Found";
+    }
+  }
+  
+  // Test your code by modifying these values
+  checkObj("gift");
