31ee03ffb259223e7c91494bc0fc8b6c683f4486
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-16 15:59:25 -0600

    Add conditional ternary operator solution

---
+++ 5-Introduction-to-JavaScript/106-Use_the_Conditional_(Ternary)_Operator.js
@@ -0,0 +1,7 @@
+// The conditional ternary operator is a short hand way of writing an if else statement.
+
+function checkEqual(a, b) {
+    return a == b ? true : false;
+  }
+  
+  checkEqual(1, 2);
