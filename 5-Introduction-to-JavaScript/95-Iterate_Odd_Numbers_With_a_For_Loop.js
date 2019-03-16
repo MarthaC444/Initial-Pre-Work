a0545f8ee3b0f1e7ee43b2ad626bba0f864fcaa0
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-15 21:45:53 -0600

    Add iterate odd numbers for loop solution

---
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -0,0 +1,16 @@
+// The for loop may be used to increment by 2's, allowing to add just odd or just even numbers to the array.
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 1; i < 10; i +=2) {
+  myArray.push(i);
+}
