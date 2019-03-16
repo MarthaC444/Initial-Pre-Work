4897a982fe0a344b279f9e1bac1d5fdc29099369
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-15 18:17:48 -0600

    Add manipulate complex objects solution

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,26 @@
+// I can store many data types in a JavaScript object.
+
+var myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [ 
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    // Add record here
+    {
+      "artist": "Pink Foot",
+      "title": "Pink Toes",
+      "release_year": 1992,
+      "formats": [
+        "CD",
+        "LP"
+      ]
+    }
+  ];
+  
