eab38c6f39653237a1e25761264155b2d78b0add
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-17 12:39:20 -0600

    Add use arrow functions for anonymous functions solution

---
+++ 6-Introduction-to-the-ES6-Challenges/6-Use-Arrow-Functions-to-Write-Concise-Anonymous-Functions.js
@@ -0,0 +1,3 @@
+// I can shorten how an anyonymous function is written. The last lesson said that const declarations should be in all caps. This solution would not pass with "const MAGIC".
+
+const magic = () => new Date();
