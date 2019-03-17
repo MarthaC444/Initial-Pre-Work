aafa605c89ff24218e8a2c06a2e01a6cf564b924
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-17 17:44:50 -0600

    Add higher order arrow functions solution

---
+++ 6-Introduction-to-the-ES6-Challenges/8-Write-Higher-Order-Arrow-Functions.js
@@ -0,0 +1,13 @@
+// Practicing with higher order arrow functions.
+
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+const squareList = (arr) => {
+  "use strict";
+  // change code below this line
+  const squaredIntegers = arr.filter(arr => arr > 0 && arr % 1 === 0).map(x => x * x);
+  // change code above this line
+  return squaredIntegers;
+};
+// test your code
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
