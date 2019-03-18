fa81263809d7bc481224ea4c1f83cd7c3280f587
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-17 18:48:48 -0600

    Add spread operator to evaluate arrays in place solution

---
+++ 6-Introduction-to-the-ES6-Challenges/11-Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js
@@ -0,0 +1,9 @@
+// The spread operator allows an array to expand in place.
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+(function() {
+  "use strict";
+  arr2 = [...arr1]; // change this line
+})();
+console.log(arr2);
