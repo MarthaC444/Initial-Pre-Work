6f2bfce0d32a7f1c910f72063e24b5c9f1ed3d61
Martha Chamberlain <mchamberlain@localhost.localdomain>
2019-03-18 13:55:57 -0600

    Add create strings using template literal solution

---
+++ 6-Introduction-to-the-ES6-Challenges/17-Create-Strings-using-Template-Literals.js
@@ -0,0 +1,23 @@
+// Template literals allow me to create and concatenate strings in different ways.
+
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["id-blacklist", "no-dup-keys"]
+  };
+  function makeList(arr) {
+    "use strict";
+  
+    // change code below this line
+    const resultDisplayArray = arr.map(strValue => `<li class="text-warning">${strValue}</li>`);
+    // change code above this line
+  
+    return resultDisplayArray;
+  }
+  /**
+   * makeList(result.failure) should return:
+   * [ `<li class="text-warning">no-var</li>`,
+   *   `<li class="text-warning">var-on-top</li>`, 
+   *   `<li class="text-warning">linebreak</li>` ]
+   **/
+  const resultDisplayArray = makeList(result.failure);
